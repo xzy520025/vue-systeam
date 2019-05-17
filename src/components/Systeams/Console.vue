1c21a8095f60ff5e95328a8c675da3c502bc4d89
guest <guest@inotesoft.com>
2019-05-17 17:53:09 +0800

    修改

--- src/components/Systeams/Console.vue
+++ src/components/Systeams/Console.vue
@@ -2,17 +2,18 @@
     <div class="Console">
         <h4 class="BoxSize">可视化数据</h4>
         <div class="Console-Nmuber">
-            <div class="Console-Nmuber-1 BoxSize">
-                1
+            <div class="Console-Nmuber-1 BoxSize Consoles1" style="margin-left: 2px;">
+                <h4>人数统计</h4>
+                <div class="Console-content-div"></div>
             </div>
-            <div class="Console-Nmuber-1 BoxSize">
-                1
+            <div class="Console-Nmuber-1 BoxSize Consoles2" style="margin-left: 5%;">
+                <h4>待办事项</h4>
             </div>
-            <div class="Console-Nmuber-1 BoxSize">
-                1
+            <div class="Console-Nmuber-1 BoxSize Consoles3" style="margin-left: 5%;">
+                <h4>系统通知</h4>
             </div>
-            <div class="Console-Nmuber-1 BoxSize">
-                1
+            <div class="Console-Nmuber-1 BoxSize Consoles4" style="margin-left: 5%;">
+                <h4>当前时间</h4>
             </div>
         </div>
     </div>
@@ -35,12 +36,42 @@ export default {
         margin: 5px 0;
     }
     .Console-Nmuber{
-        overflow: hidden;
+        /* overflow: hidden; */
     }
     .Console-Nmuber-1{
         width: 20%;
         float: left;
-        margin: 5px 2%;
+        color: #fff;
+        padding: 15px 10px;
+        padding-top: 10px;
+    }
+    .BoxSize h4{
+        font-weight: 600;
+        position: relative;
+    }
+    .BoxSize h4:after{
+        content:' ';
+        height : 1px;
+        width: 100%;
+        background: #fff;
+        position: absolute;
+        bottom: -5px;
+        left: 0px;
+    }
+    .Console-content-div{
+        
+    }
+    .Consoles1{
+        background: cadetblue;
+    }
+    .Consoles2{
+        background: skyblue;
+    }
+    .Consoles3{
+        background: sienna;
+    }
+    .Consoles4{
+        background: chocolate;
     }
 </style>
 
